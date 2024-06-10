1244a92890cee248752b03e5a2f608afeb4e6be2
Adrian Ruiz <adrian.ruiz@singulardevs.com>
2024-06-10 13:56:19 +0200

    add post install script

---
+++ scripts/postinstall.ts
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import fs from 'fs'
+import path from 'path'
+
+const filesToMove = ['../src']
+const packageDir = __dirname
+const rootDir = path.join(packageDir, '../../../')
+
+filesToMove.forEach(file => {
+  const srcPath = path.join(packageDir, file);
+  const destPath = path.join(rootDir, file);
+
+  if (fs.existsSync(srcPath)) {
+    fs.copyFileSync(srcPath, destPath);
+    console.log(`Archivo ${file} movido a ${destPath}`);
+  } else {
+    console.error(`El archivo ${file} no existe en ${srcPath}`);
+  }
+});
