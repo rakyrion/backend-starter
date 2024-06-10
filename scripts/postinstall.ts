ff3d6797bfb462ce6e71e8ae4fc65867d5e02b65
Adrian Ruiz <adrian.ruiz@singulardevs.com>
2024-06-10 14:00:40 +0200

    change import to require on postinstall

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import fs from 'fs'
-import path from 'path'
+const fs = require('fs');
+const path = require('path');
 
 const filesToMove = ['../src']
 const packageDir = __dirname
