a85ab57934f8a165ebaa62f2b5d1d81e71da809b
Adrian Ruiz <adrian.ruiz@singulardevs.com>
2024-06-12 10:38:45 +0200

    fix

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,8 +2,3 @@ import '../core'
 import '../config'
 import './config'
 import './init'
-import { BaseRepository } from './repositories/base'
-
-module.exports = {
-	BaseRepository
-}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 // Run express application by default
 import './app'
+import { BaseRepository } from './database/repositories/base'
+
+module.exports = {
+	BaseRepository
+}
