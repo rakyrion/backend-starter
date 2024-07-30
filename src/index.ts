f125a7db10b1bd6c7882e132dbb906a8ce7f6df3
Adrian Ruiz <adrian.ruiz@singulardevs.com>
2024-07-30 10:17:30 +0200

    add app exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './database/exports'
 export * from './email/exports'
 export * from './auth/exports'
 export * from './core/exports'
+export * from './app/exports'
 
 // Run express application by default
 import './app'
